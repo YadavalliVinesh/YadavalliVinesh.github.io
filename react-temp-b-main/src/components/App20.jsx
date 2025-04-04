95569b35939b3f295e07774c33ff0a2fa5c7a7e0
YadavalliVinesh <yadavallivinesh18@gmail.com>
2025-04-04 14:48:03 +0530

    modified

---
+++ react-temp-b-main/src/components/App20.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class App20 extends Component {
+  render() {
+    return (
+      <div>
+        <h3>This is App20</h3>
+        <p>This is just to lean the syntax of class component</p>
+      </div>
+    )
+  }
+}
+
