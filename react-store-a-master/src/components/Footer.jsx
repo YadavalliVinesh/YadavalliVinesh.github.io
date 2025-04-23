3ee207ffc11d089bc745a0f0429d5889e818047a
YadavalliVinesh <yadavallivinesh18@gmail.com>
2025-04-23 09:51:05 +0530

    Delete react-store-a-master directory

--- react-store-a-master/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <div style={{backgroundColor:'silver'}}>
-      @Copyright 2025. All rights reserved.
-    </div>
-  )
-}
